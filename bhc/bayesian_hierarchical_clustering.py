385b09c6d4bb88bb578322e461b81615a7ca5c06
Sam Voisin <psv6@duke.edu>
2020-07-06 12:48:54 -0400

    further development on BHC and Cluster objects

--- bhc/bayesian_hierarchical_clustering.py
+++ bhc/bayesian_hierarchical_clustering.py
@@ -8,7 +8,7 @@ Class `BHC`. This is the primary object for user interface in the bhc library.
 """
 
 import numpy as np
-from scipy import linalg as la
+from cluster import Cluster
 
 
 class BHC:
@@ -16,20 +16,46 @@ class BHC:
     This is the primary object for user interface in the bhc library.
     """
 
-    def __init__(self, x: np.ndarray, alpha: float, prior_params: dict, family="univariate_gaussian"):
+    def __init__(self, data: np.ndarray, alpha: float, prior_params: dict, family="normal_in_gamma"):
         """
-        initialize the bhc algorithm with the data set and hyper-parameters
+        Initialize the bhc object with the data set and hyper-parameters.
 
-        :param x: an `n` by `d` array of data to be clustered where n is number of data points and d is dimension
+        :param data: an `n` by `d` array of data to be clustered where n is number of data points and d is dimension
         :param alpha: cluster concentration hyper-parameter
         :param prior_params: a dictionary of hyper-parameters for prior distributions
         :param family: distribution family to be used; this must match the prior parameter dictionary provided
         """
-        self.x = x
-        self.data_dims = x.shape
+        self.data = data
+        self.n_data, self.n_dims = data.shape
         self.alpha = alpha
         self.prior_params = prior_params
         self.family = family
         # n by n table for storing posterior merge probabilities
-        self.post_table = np.zeros(shape=(self.data_dims[0], self.data_dims[0]))
+        self.posterior_table = np.zeros(shape=(self.n_data, self.n_dims))
+        self.uf_array = np.arange(self.n_data)  # union-find tracking array
+        self.clusters = {n: Cluster(c, self.alpha, n) for n, c in enumerate(data)}  # init points as individual clusters
 
+    def _find(self):
+        """
+        Find the pair of clusters with the highest posterior merge probability
+        """
+        max_coords = np.where(self.posterior_table == self.posterior_table.max())
+        clusti, clustj = self.uf_array[max_coords]  # get cluster labels; this does not return Cluster objects
+        return clusti, clustj
+
+    def _union(self, i: int, j: int):
+        """
+        Joint the pair of clusters with highest posterior merge probability
+
+        :param i: integer label for cluster i
+        :param j: integer label for cluster j
+        """
+        pass
+
+
+if __name__ == "__main__":
+    x = np.arange(24).reshape(12, 2)
+    tree = BHC(x, 0.1, {"tst": 1})
+    for k, v in tree.clusters.items():
+        print(f"points: {v.points}")
+        print(f"labels: {v.label}")
--- bhc/cluster.py
+++ bhc/cluster.py
@@ -10,12 +10,22 @@ Class `Cluster` representing a cluster of data points. This the fundamental comp
 import numpy as np
 
 
+def _calc_merge_posterior(clusti, clustj):
+    """
+    calculate the posterior probability of merging with another cluster
+
+    :param clusti: Cluster class instance
+    :param clustj: Cluster class instance
+    """
+    pass
+
+
 class Cluster:
     """
     Object representing a cluster of data points. This the fundamental element of the union-find structure.
     """
 
-    def __init__(self, x: float or np.ndarray, alpha, label: int):
+    def __init__(self, x: float or np.ndarray, alpha: float, label: int):
         """
         initialize cluster for a single data point. On initialization each cluster is a singleton set (i.e. a leaf).
 
@@ -30,5 +40,3 @@ class Cluster:
         # d parameter controls merge hypothesis prior using information in subtrees; it represents the observation that
         # subtrees which were not merged on previous iterations may not share a distribution
         self.d = alpha
-
-
