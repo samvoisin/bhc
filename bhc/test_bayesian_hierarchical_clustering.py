77bebbcb468e7f068a1469faaf3b47a23259d885
samvoisin <sam.voisin@geomdata.com>
2020-07-21 08:44:56 -0400

    Added a few minor lines to `test_bayesian_hierarchical_clustering`

--- bhc/test_bayesian_hierarchical_clustering.py
+++ bhc/test_bayesian_hierarchical_clustering.py
@@ -10,6 +10,7 @@ test methods for Cluster class
 
 import pytest
 import numpy as np
+from scipy.stats import multivariate_normal
 from bhc.bayesian_hierarchical_clustering import BHC
 
 class TestBHC:
@@ -21,7 +22,12 @@ class TestBHC:
         """
         initialize data needed to perform testing
         """
-        pass
+        np.random.seed(1)
+        self.test_data = multivariate_normal.rvs(mean=np.zeros(3), cov=np.eye(3), size=10)
+        self.alpha = 1
+        self.params1 = {}  # to be included; params1 is parameterization for mvt normal case
+        self.params2 = {}  # to be included; params2 is parameterization for univariate extension case
+        self.params3 = {}  # to be included; params3 is parameterization for dirichlet multinomial case
 
     @pytest.xfail()
     def test_bhc_init(self):
